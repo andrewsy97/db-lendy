7b2a4fcf404a03ffe9340e346d6a19f7ffea2da3
andrewsy97 <andrewsy97@gmail.com>
2017-04-11 15:57:39 -0400

    added a frontend landing page

--- lendy/config/routes.rb
+++ lendy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'grayscales#index'
+
   namespace :admin do
     resources :users
     resources :items
@@ -18,7 +20,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
