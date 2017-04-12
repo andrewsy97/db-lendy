c97347a79a3665b69d49956e859a1f31bea2a02c
andrewsy97 <andrewsy97@gmail.com>
2017-04-12 15:36:03 -0400

    added nicer views

--- lendy/config/routes.rb
+++ lendy/config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   resources :items
   resources :item_types
   resources :schools
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
